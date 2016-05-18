a2efcad17a72fead0586a14bc1da0ef3bbc551e5
Thai Long Tran <long@boldradius.com>
2016-05-17 20:28:09 -0400

    updates

--- README.md
+++ README.md
@@ -9,9 +9,19 @@ In the root of this project, execute "./run"
 
 
 
-REPL Version
+ML - REPL Version
 ============
 
 There is a script / REPL version you can copy and paste.
-This is in src-script/ciena.ml.scala
+Make sure data.txt is in the correct folder.
+This is in src-script/stats.ml.scala
+
+DataFrames - REPL Version
+============
+
+There is a script / REPL version you can copy and paste.
+Make sure data.txt is in the correct folder.
+This is in src-script/stats.dataframes.scala
+- start your spark shell with the databricks CSV library
+  - ./spark-shell --packages com.databricks:spark-csv_2.11:1.4.0
 
--- src-script/stats.dataframes.scala
+++ src-script/stats.dataframes.scala
@@ -1,54 +1,42 @@
 import org.apache.spark.sql._
 
-case class statobject(one: Double, two: Double, three:Double, four:Double)
-
-def toDouble(s: String, default: Double = -1): Double = try {
-  val s2 = s.trim
-  if (s2.length == 0 || s2 == "NA") default
-  else s2.toDouble
-} catch {
-  case nfe: NumberFormatException =>
-    Console.err.println(s"""NFE: "$s".toDouble""")
-    default
-}
+val df = sqlContext.read
+  .format("com.databricks.spark.csv")
+  .option("header", "false") // Use first line of all files as header
+  .option("inferSchema", "true") // Automatically infer data types
+  .option("delimiter", " ")
+  .load("data/data.txt")
+  .cache
 
 
-def statobjectParse(in:Array[String]):statobject = {
-  statobject(toDouble(in(0)), toDouble(in(1)), toDouble(in(2)), toDouble(in(3)))}
-//try this:  https://github.com/databricks/spark-csv
-val rawdata = sc.textFile("data/data.txt").
-  map(x => x.split(" ")).
-  map(x => statobjectParse(x)).
-  toDF.
-  cache
+//this will print out count, mean, stddev, min, max
+df.describe().show
 
-//try some of these: https://databricks.com/blog/2015/06/02/statistical-and-mathematical-functions-with-dataframes-in-spark.html
-//df.describe().show()  will show mean and stddev
-//df.stat.corr('id', 'id') instead of df.agg(corr(col1, col2))
-
+//an alternate way to get the means
 println("the average of each column is as follows:")
-//todo - get all columns and pass 'em
-rawdata.agg(mean("one"), mean("two"), mean("three"), mean("four")).show
+rawdata.agg(mean("c1"), mean("c2"), mean("c3"), mean("c4")).show
 
+//an alternate way to get the standard deviations
 println("The standard deviation for each column is as follows:")
-//todo - get all columns and pass 'em
-rawdata.agg(stddev_samp("one"), stddev_samp("two"), stddev_samp("three"), stddev_samp("four")).show
+rawdata.agg(stddev_samp("c1"), stddev_samp("c2"), stddev_samp("c3"), stddev_samp("c4")).show
 
 
+//function to create matrix
 def corrMatrix(data:DataFrame) : Array[Array[Any]] = {
   def getCorr(col1: String, col2: String) :String = {
-    rawdata.agg(corr(col1, col2)).take(1)(0)(0).toString
+    if (col1 == col2) "1.0"
+    else data.agg(corr(col1, col2)).take(1)(0)(0).toString
   }
   val colNames = data.columns
   val colNameMatrix = colNames.map(x => colNames.map(y => List(y, x).sortBy(z => z)))
   val setOfNames = colNameMatrix.
-    flatten.
-    toSet
+                          flatten.
+                          toSet
   val mapOfCorrelations = setOfNames.map(x => (x.mkString(""), getCorr(x(0), x(1)))).
-    toMap
+                                    toMap
 
-  colNameMatrix.map(x => x.map(y => abc.getOrElse(y.mkString(""),0)))
+  colNameMatrix.map(x => x.map(y => mapOfCorrelations.getOrElse(y.mkString(""),0)))
 }
 
 //correlation matrix:
-corrMatrix(rawdata).foreach(z => println(z.toList))
+corrMatrix(df).foreach(z => println(z.toList))
